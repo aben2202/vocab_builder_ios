b37a4f1d8f3c4c72769abe72c71731aa8c25ae67
Andrew Benson <andebenson@gmail.com>
2013-07-09 13:41:57 -0500

    added review controller

---
+++ vocab_builder/vocab_builder/ReviewViewController.m
@@ -0,0 +1,43 @@
+//
+//  ReviewViewController.m
+//  vocab_builder
+//
+//  Created by andebenson on 7/9/13.
+//  Copyright (c) 2013 andebenson. All rights reserved.
+//
+
+#import "ReviewViewController.h"
+
+@interface ReviewViewController ()
+
+@end
+
+@implementation ReviewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)yesButtonClicked:(id)sender {
+}
+
+- (IBAction)noButtonClicked:(id)sender {
+}
+@end
